0f243fe1247c171d4eb756fc35e779014d84a5a5
NABEEL004 <66759134+NABEEL004@users.noreply.github.com>
2023-10-21 17:31:11 +0800

    added typewriter effect to landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/brown_guy.png'; 
+import Typewriter from 'typewriter-effect';
 
 export default function Landing() {
+    const [display, setDisplay] = useState(false)
   return (
     <div className="flex justify-center items-center p-1 sm:p-3 md:p-6 bg-[#D4A269] absolute top-0 left-0 h-full w-full">
-        <div className='flex flex-col mx-10 justify-center items-center sm:items-start'>
-            <div className = "text-5xl sm:text-6xl md:text-8xl font-title font-black text-[#FDFAE3] m-5">
-                hi, <br/> i'm Nabeel :-)
+        <div className='flex flex-col mx-10 justify-between items-center sm:items-start w-[800px]'>
+            <div className = "text-5xl sm:text-6xl md:text-8xl font-title font-black text-[#FDFAE3] m-5 h-[200px]">
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter.typeString("hi, <br> i'm Nabeel :-)")
+                        .callFunction(() => {
+                            console.log('String typed out!');
+                        })
+                        .start()
+                        .callFunction(() => {
+                            setDisplay(true)
+                        }
+                        );
+                    }}
+                />
+                {/* hi, <br/> i'm Nabeel :-) */}
             </div>
-            <div className="">
+            <div className="h-[40px]">
                 <Link to='/about'>
-                    <button className="font-title font-black bg-[#FDFAE3] rounded-full p-5 m-5 px-10 text-[#D4A269] text-lg sm:text-xl md:text-3xl hover:scale-105 active:scale-95 transition">Don't Click This!</button>
+                    <button className={`${display ? 'opacity-100' : 'opacity-0'} font-title font-black bg-[#FDFAE3] rounded-full p-5 m-5 px-10 text-[#D4A269] text-lg sm:text-xl md:text-3xl hover:scale-105 active:scale-95 transition duration-200`}>Don't Click This!</button>
                 </Link>
             </div>
         </div>
